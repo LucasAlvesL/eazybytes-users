1ad548e12ee65ca754047514c65e03b57b1cf621
Lucas Alves Lobo <59810995+LucasAlvesL@users.noreply.github.com>
2025-07-01 16:28:26 -0300

    Feat: Redis for cache profile infos

--- src/main/java/com/eazybytes/services/CustomerProfileService.java
+++ src/main/java/com/eazybytes/services/CustomerProfileService.java
@@ -2,6 +2,7 @@ package com.eazybytes.services;
 
 import com.eazybytes.dtos.CustomerProfileResponseDTO;
 import com.eazybytes.repository.CustomerRepository;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -10,15 +11,24 @@ import java.util.UUID;
 public class CustomerProfileService {
 
     private final CustomerRepository repository;
+    private final RedisTemplate<String, Object> redisTemplate;
 
-    public CustomerProfileService(CustomerRepository repository) {
+    public CustomerProfileService(CustomerRepository repository, RedisTemplate<String, Object> redisTemplate) {
         this.repository = repository;
+        this.redisTemplate = redisTemplate;
     }
 
     public CustomerProfileResponseDTO execute(UUID customerId) {
+        String redisKey = "customer:profile:" + customerId;
+
+        CustomerProfileResponseDTO cachedProfile = (CustomerProfileResponseDTO) redisTemplate.opsForValue().get(redisKey);
+        if (cachedProfile != null) {
+            return cachedProfile;
+        }
+
         var customer = this.repository.findById(customerId).orElseThrow(() -> new IllegalArgumentException("Customer not found"));
 
-        return CustomerProfileResponseDTO.builder()
+        CustomerProfileResponseDTO profile = CustomerProfileResponseDTO.builder()
                 .firstName(customer.getFirstName())
                 .middleName(customer.getMiddleName())
                 .lastName(customer.getLastName())
@@ -27,6 +37,10 @@ public class CustomerProfileService {
                 .accountNumber(customer.getAccountNumber())
                 .accountType(customer.getAccountType())
                 .build();
+
+        redisTemplate.opsForValue().set(redisKey, profile);
+
+        return profile;
     }
 
 }
